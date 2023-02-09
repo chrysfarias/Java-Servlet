fcb507a35b9ba2702d92823d8f922ef463918b50
Chrystian <chrystianfarias@hotmail.com>
2023-02-09 14:49:33 -0400

    first servlet

---
+++ src/main/java/package1/HelloWorldServlet.java
@@ -0,0 +1,29 @@
+package package1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out =  resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("hello, world, congratz");
+		out.println("</body");
+		out.println("</html");
+		
+	}
+
+}
